b67f499542c369d29bc16134dfcec333f7f9e708
Mahesward <maheaswerd@gmail.com>
2023-01-27 18:00:47 +0530

    Confict solved

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -3,20 +3,12 @@ import './NavBar.css'
 
 function NavBar() {
   return (
-<<<<<<< HEAD
-    <div className="navbar">
-      <h4 className='logo'>Quizo</h4>
-      <img src="/img/Profile-Demo.jpg" alt="img" className="profile-img" />
-      <div className="line-below" />
-    </div>
-=======
     <>
       <div className="navbar">
         <h4 className='logo'>Quizo</h4>
         <img src="/img/Profile-Demo.jpg" alt="img" className="profile-img" />
       </div>
     </>
->>>>>>> 689bbeaf31debc67d7cb035db913ecd0cd853616
   )
 }
 
