7cffe7442b28ee15f8883e2cac9dd9ac4134a5f7
remintroy <reminremin01@gmail.com>
2023-01-27 19:20:43 +0530

    SideBar update

--- client/src/components/SideBar/SideBar.jsx
+++ client/src/components/SideBar/SideBar.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { useContext } from "react";
 import { useState } from "react";
+import User from "../../context/User";
 import "./SideBar.css";
 
 function SideBar() {
-  const [showSideBar, setShowSideBar] = useState(true);
+  const [showSideBar, setShowSideBar] = useState(false);
+  const userContext = useContext(User);
 
   return (
     <div className={`SideBar ${showSideBar ? "show" : ""}`}>
@@ -13,37 +16,48 @@ function SideBar() {
         </div>
         <ul className="actions">
           <li>
-            <span className="material-symbols-outlined">home</span> Home
+            <span className="material-symbols-outlined" onClick={(e) => setShowSideBar(!showSideBar)}>
+              home
+            </span>{" "}
+            {showSideBar && "Home"}
           </li>
           <li>
-            <span className="material-symbols-outlined">dataset</span> Projects
+            <span className="material-symbols-outlined">dataset</span> {showSideBar && "Projects"}
           </li>
           <li>
-            <span className="material-symbols-outlined">task</span> Tasks
+            <span className="material-symbols-outlined">task</span>
+            {showSideBar && "Tasks"}
           </li>
           <li>
-            <span className="material-symbols-outlined">stars</span> Stared
+            <span className="material-symbols-outlined">stars</span>
+            {showSideBar && "Stared"}
           </li>
         </ul>
       </div>
       <div className="bottom">
         <ul className="actions">
           <li>
-            <span className="material-symbols-outlined">settings</span> Settings
+            <span className="material-symbols-outlined">settings</span> {showSideBar && "Settings"}
           </li>
           <li>
-            <span className="material-symbols-outlined">contact_support</span>Support
+            <span className="material-symbols-outlined">contact_support</span> {showSideBar && "Support"}
           </li>
         </ul>
-        <div className="userData">
-          <img src="/img/Profile-Demo.jpg" alt="img" className="profile-img" />
-          {showSideBar && (
-            <div className="userInfo">
-              <b>Name</b>
-              <p>sample@email.com</p>
-            </div>
-          )}
-        </div>
+        {userContext?.user && (
+          <div className="userData">
+            <img src="/img/Profile-Demo.jpg" alt="img" className="profile-img" />
+            {showSideBar && (
+              <div className="userInfo">
+                <b>{userContext?.user?.name}</b>
+                <p>
+                  {userContext?.user?.email?.length > 15
+                    ? `${userContext?.user?.email.slice(0, 15)}...`
+                    : userContext?.user?.email}
+                </p>
+              </div>
+            )}
+          </div>
+        )}
       </div>
     </div>
   );
