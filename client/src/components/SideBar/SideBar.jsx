2f31df52845e6a48bfc6649a8a4918c0bde34e63
remintroy <reminremin01@gmail.com>
2023-01-31 12:26:04 +0530

    Sidebar and Mobile Bottom NavbarUpdate

--- client/src/components/SideBar/SideBar.jsx
+++ client/src/components/SideBar/SideBar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useContext } from "react";
+import { useEffect } from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import User from "../../context/User";
@@ -7,69 +8,96 @@ import "./SideBar.css";
 
 function SideBar(PageContent) {
   const [show, setShow] = useState(true);
+  const [isMobile, setIsMoblie] = useState(true);
   const userContext = useContext(User);
+
+  useEffect(() => {
+    window.addEventListener("resize", (e) => {
+      setIsMoblie(window.innerWidth < 600);
+      setShow(window.innerWidth > 1000);
+    });
+    return () => window.removeEventListener("resize", () => console.log("event removed"));
+  }, []);
 
   const navigate = useNavigate();
 
   const path = window.location.pathname;
 
   return (
-    <div className={`SideBar ${show ? "show" : ""}`}>
-      <div className="Bar">
-        <div className="top">
-          <div className="logoSection" onClick={() => setShow(!show)}>
-            <img src="/logo.png" alt="" />
-            {show && <h2>Quizo</h2>}
-          </div>
-
-          <div className="searchBar">
-            <input type="text" placeholder="Search" />
-          </div>
-
-          <ul className="actions">
-            <li className={path === "/" ? "on" : ""} onClick={() => navigate("/")}>
-              <span className="material-symbols-outlined">home</span>
-              {show && "Home"}
-            </li>
-            <li className={path === "/projects" ? "on" : ""} onClick={() => navigate("/projects")}>
-              <span className="material-symbols-outlined">dataset</span> {show && "Projects"}
-            </li>
-            <li className={path === "/tasks" ? "on" : ""} onClick={() => navigate("/tasks")}>
-              <span className="material-symbols-outlined">task</span> {show && "Tasks"}
-            </li>
-            <li className={path === "/stared" ? "on" : ""} onClick={() => navigate("/stared")}>
-              <span className="material-symbols-outlined">stars</span> {show && "Stared"}
-            </li>
-          </ul>
-        </div>
-
-        <div className="bottom">
-          <ul className="actions">
-            <li className={path === "/settings" ? "on" : ""} onClick={() => navigate("/settings")}>
-              <span className="material-symbols-outlined">settings</span> {show && "Settings"}
-            </li>
-            <li className={path === "/support" ? "on" : ""} onClick={() => navigate("/support")}>
-              <span className="material-symbols-outlined">contact_support</span> {show && "Support"}
-            </li>
-          </ul>
-
-          {userContext?.user && (
-            <div className="userData">
-              <img src="/img/Profile-Demo.jpg" alt="img" className="profile-img" />
-              {show && (
-                <div className="userInfo">
-                  <b>{userContext?.user?.name}</b>
-                  <p>
-                    {userContext?.user?.email?.length > 15
-                      ? `${userContext?.user?.email.slice(0, 15)}...`
-                      : userContext?.user?.email}
-                  </p>
-                </div>
-              )}
+    <div className={`SideBar ${show ? `show` : ""} ${isMobile ? "mb" : ""}`}>
+      {!isMobile && (
+        <div className="Bar">
+          <div className="top">
+            <div className="logoSection" onClick={() => setShow(!show)}>
+              <img src="/logo.png" alt="" />
+              {show && <h2>Quizo</h2>}
             </div>
-          )}
+
+            <div className="searchBar">
+              <input type="text" placeholder="Search" />
+            </div>
+
+            <ul className="actions">
+              <li className={path === "/" ? "on" : ""} onClick={() => navigate("/")}>
+                <span className="material-symbols-outlined">home</span>
+                {show && "Home"}
+              </li>
+              <li className={path === "/projects" ? "on" : ""} onClick={() => navigate("/projects")}>
+                <span className="material-symbols-outlined">dataset</span> {show && "Projects"}
+              </li>
+              <li className={path === "/tasks" ? "on" : ""} onClick={() => navigate("/tasks")}>
+                <span className="material-symbols-outlined">task</span> {show && "Tasks"}
+              </li>
+              <li className={path === "/stared" ? "on" : ""} onClick={() => navigate("/stared")}>
+                <span className="material-symbols-outlined">stars</span> {show && "Stared"}
+              </li>
+            </ul>
+          </div>
+
+          <div className="bottom">
+            <ul className="actions">
+              <li className={path === "/settings" ? "on" : ""} onClick={() => navigate("/settings")}>
+                <span className="material-symbols-outlined">settings</span> {show && "Settings"}
+              </li>
+              <li className={path === "/support" ? "on" : ""} onClick={() => navigate("/support")}>
+                <span className="material-symbols-outlined">contact_support</span> {show && "Support"}
+              </li>
+            </ul>
+
+            {userContext?.user && (
+              <div className="userData">
+                <img src="/img/Profile-Demo.jpg" alt="img" className="profile-img" />
+                {show && (
+                  <div className="userInfo">
+                    <b>{userContext?.user?.name}</b>
+                    <p>
+                      {userContext?.user?.email?.length > 15
+                        ? `${userContext?.user?.email.slice(0, 15)}...`
+                        : userContext?.user?.email}
+                    </p>
+                  </div>
+                )}
+              </div>
+            )}
+          </div>
         </div>
-      </div>
+      )}
+      {isMobile && (
+        <ul className="BarMB">
+          <li className={path === "/" ? "on" : ""} onClick={() => navigate("/")}>
+            <span className="material-symbols-outlined">home</span>
+          </li>
+          <li className={path === "/projects" ? "on" : ""} onClick={() => navigate("/projects")}>
+            <span className="material-symbols-outlined">dataset</span>
+          </li>
+          <li className={path === "/tasks" ? "on" : ""} onClick={() => navigate("/tasks")}>
+            <span className="material-symbols-outlined">task</span>
+          </li>
+          <li className={path === "/settings" ? "on" : ""} onClick={() => navigate("/settings")}>
+            <span className="material-symbols-outlined">settings</span>
+          </li>
+        </ul>
+      )}
       <div className="PageContent">{PageContent.children}</div>
     </div>
   );
